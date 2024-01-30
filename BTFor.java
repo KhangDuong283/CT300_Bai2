9d6e670ad52f33d1c699cb38c11e5a675992cbe3
Khang Duong <khangb2111802@student.ctu.edu.vn>
2024-01-30 09:44:42 +0700

    BTFor.java

---
+++ BTFor.java
@@ -0,0 +1,8 @@
+public class BTFor {
+    public static void main(String[] args) {
+        // Sử dụng vòng lặp for để hiển thị các số từ 1 đến 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
